20b0feee60dd42ef7e35e2e738ea1e87c1642d11
남궁성빈 <k971711@naver.com>
2025-06-07 13:36:35 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HeaderSection from './components/HeaderSection/HeaderSection.js';
 import MainSection from './components/MainSection/MainSection.js';
 
 function App() {
   return (
-    <div className="App">
-      <HeaderSection />
-      <MainSection />
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <HeaderSection />
+        <Routes>
+          <Route path="/" element={<MainSection />} />
+          {/* 다른 라우트 예시 */}
+          {/* <Route path="/about" element={<About />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
